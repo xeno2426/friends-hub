ca9ccefc875e94987e54d9e625d37dc807a28d7e
xeno2426 <manthanbhoyar42@gmail.com>
2026-02-14 13:19:17 +0530

    Create Post model schema

---
+++ backend/models/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Post', postSchema);
